972a3d7254111e04af6bdf96faeda7033d47afed
Deinyefa Eporwei <df.eporwei@gmail.com>
2018-03-16 21:23:14 -0400

    finishing off edit profile styles, added react-native-simple-radio-button

--- src/components/subscreens/EditProfile.js
+++ src/components/subscreens/EditProfile.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, View, Button, Image, TextInput } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
+import RadioForm, {
+  RadioButton,
+  RadioButtonInput,
+  RadioButtonLabel
+} from 'react-native-simple-radio-button';
 
 import { Input } from '../common';
+
+let radio_props = [
+  { label: 'Female', value: 'female' },
+  { label: 'Male', value: 'male' },
+  { label: 'Prefer not to say', value: 'n/s' }
+];
 
 class EditProfile extends Component {
   static navigationOptions = {
@@ -41,31 +52,51 @@ class EditProfile extends Component {
           <Text style={{ color: 'gray', fontSize: 11, marginBottom: 20 }}>
             General Information
           </Text>
-          <View>
-            <View style={{ flexDirection: 'row' }}>
-              <Text style={styles.labelStyles}>Name</Text>
-              <TextInput style={styles.inputStyles} />
-            </View>
-            <View style={{ flexDirection: 'row' }}>
-              <Text style={styles.labelStyles}>Username</Text>
-              <TextInput style={styles.inputStyles} />
-            </View>
-            <View style={{ flexDirection: 'row' }}>
-              <Text style={styles.labelStyles}>Bio</Text>
-              <TextInput style={styles.inputStyles} />
-            </View>
-            <View style={{ flexDirection: 'row' }}>
-              <Text style={styles.labelStyles}>Location</Text>
-              <TextInput style={styles.inputStyles} />
-            </View>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={styles.labelStyles}>Name</Text>
+            <TextInput style={styles.inputStyles} />
+          </View>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={styles.labelStyles}>Username</Text>
+            <TextInput style={styles.inputStyles} />
+          </View>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={styles.labelStyles}>Bio</Text>
+            <TextInput style={styles.inputStyles} />
+          </View>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={styles.labelStyles}>Location</Text>
+            <TextInput style={styles.inputStyles} />
           </View>
         </View>
         <View style={styles.informationStyles}>
           <Text style={{ color: 'gray', fontSize: 11, marginBottom: 20 }}>
             Private Information
           </Text>
-          <Input label="Email" />
-          <Input label="Gender" />
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={styles.labelStyles}>Email</Text>
+            <TextInput style={styles.inputStyles} />
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              paddingTop: 20
+            }}
+          >
+            <Text>Gender</Text>
+            <RadioForm
+              accessible={true}
+              radio_props={radio_props}
+              initial={0}
+              formHorizontal={false}
+              labelHorizontal={true}
+              buttonColor={'#E35D91'}
+              animation={true}
+              onPress={value => console.log(value + ' has been selected')}
+              style={{ borderLeftWidth: 2, borderLeftColor: '#000' }}
+              buttonWrapStyle={{ marginLeft: 20 }}
+            />
+          </View>
         </View>
       </View>
     );
@@ -101,8 +132,7 @@ styles = {
   },
   labelStyles: {
     fontSize: 13,
-    flex: 1,
-    marginBottom: -5
+    flex: 1
   }
 };
 
